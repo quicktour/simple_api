682ac2acadeb9c8d768305af17905a68c8c5465c
Viktor Kuras <kurasviktor@gmail.com>
2020-12-11 12:27:22 +0300

    Update response

--- app/controllers/data_sources_controller.rb
+++ app/controllers/data_sources_controller.rb
@@ -7,9 +7,11 @@ class DataSourcesController < ApplicationController
 
   def resp_data
     {
-      first_name: 'Олег',
-      last_name: 'Вещий',
-      university: 'Киевский университет им Т. Г. Шевченко'
+      user_id: 12,
+      mode_study: 1231231,
+      academic_year_id: 1234,
+      portfolio: 'wehfwjhbjwhbcjebch',
+      number: 'dqwqwdqwddw'
     }
   end
 end
